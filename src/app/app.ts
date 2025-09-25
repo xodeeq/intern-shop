8127b2c230eb71c11d16d354758efbf732862c2a
Opakunle Kingsley <kingsleyopakingsleyopakunle@gmail.com>
2025-09-25 13:44:23 +0100

    Made ProductSearch standalone and added Output event for communication

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductSearch } from './components/product-search/product-search';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductSearch],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
+    <h1 class="header">Welcome to Products Store!</h1>
+    <app-product-search (productsearched)="handleProductSearch($event)"></app-product-search>
     <router-outlet />
+    <style>
+      
+    </style>
   `,
-  styles: [],
+  styles: [`
+    .header { 
+        text-align: center;
+        color: #2c3e50;
+        font-family: Arial, sans-serif;
+        margin-top: 20px;
+      }
+    `],
 })
 export class App {
   protected readonly title = signal('intern-shop');
+
+  handleProductSearch(event: string) {
+    console.log('Product searched:', event);
+  }
 }
